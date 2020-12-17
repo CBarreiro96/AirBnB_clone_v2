c55c7449c4620150c5023147716ebf5f1049c71a
matcls <manueltorresvesga@gmail.com>
2020-12-16 21:20:36 -0500

    Add PrimaryKey

--- models/user.py
+++ models/user.py
@@ -11,11 +11,11 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
-    places = relationship("Place", cascade='all, delete',
+    places = relationship("Place", cascade='delete',
                           backref='user')
 
-    reviews = relationship("Review", cascade='all, delete',
+    reviews = relationship("Review", cascade='delete',
                            backref='user')
