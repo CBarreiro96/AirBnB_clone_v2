6ecc83d1b05a10c96f41e2d25dcb6a40961a302c
Camilo Barreiro <1845@holbertonschool.com>
2021-01-21 00:29:17 -0500

    HBN filters

--- models/state.py
+++ models/state.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
-from models.base_model import Base, BaseModel
-import models
+#!/usr/bin/python3
+"""This is the state class"""
+from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
+import models
+from os import environ
 
 
 class State(BaseModel, Base):
-    """State class."""
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    cities = relationship('City', backref='state', cascade='delete')
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
 
+        @property
+        def cities(self):
+            """ Returns the list of City instances with
+            state_id == current State.id """
+            all_cities = models.storage.all(City)
+            state_cities = []
+            for city_ins in all_cities.values():
+                if city_ins.state_id == self.id:
+                    state_cities.append(city_ins)
 
-if getenv("HBNB_TYPE_STORAGE") != "db":
-    @property
-    def cities(self):
-        """Return the list of City instances with state_id equals
-        to the current State.id."""
-        cities = []
-        for obj in models.storage.all(City).items():
-            if obj.state_id == self.id:
-                cities.append(obj)
-        return cities
+            return state_cities
