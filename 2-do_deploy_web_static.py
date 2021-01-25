fcb69b8ca0954ddfc55fd5555d03b9ab3f6ae119
Camilo Andrés Barreiro Herrera <66263776+CBarreiro96@users.noreply.github.com>
2021-01-25 09:23:58 -0500

    Fix output

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,46 +3,29 @@
 A Fabric script (based on the file 1-pack_web_static.py) that distributes
 an archive to my web servers, using the function do_deploy.
 """
-import os.path
+from os.path import exists
 from fabric.api import env, put, run
-from fabric.operations import run, put, sudo
 
 """my server's ip addresses"""
 env.hosts = ['35.185.122.18', '35.237.15.1']
 
 
-def do_pack():
-    """packages all contents from web_static into .tgz archive
-    """
-    date = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-              format(date))
-        return ("versions/web_static_{:s}.tgz".format(date))
-    except:
-        return None
-
-
 def do_deploy(archive_path):
-    """do_deploy function."""
-    if (os.path.isfile(archive_path) is False):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        file_name = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + file_name.split(".")[0])
-
-        put(archive_path, "/tmp")
-        run("mkdir -p {}".format(new_folder))
-        run("tar -xzf /tmp/{} -C {}".
-            format(file_name, new_folder))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("rm -rf {}/web_static".format(new_folder))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run("ln -s {} /data/web_static/current".format(new_folder))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
     except:
         return False
